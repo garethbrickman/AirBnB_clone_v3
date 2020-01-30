f88f7af2a1eaf883fc4289b236d8bcc07ef843e5
Gareth Brickman <977@holbertonschool.com>
2020-01-30 03:34:57 +0000

    [TASK] Create places_amenities file, import it in __innit__

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -14,3 +14,4 @@ if True:
     from api.v1.views.users import *
     from api.v1.views.places import *
     from api.v1.views.places_reviews import *
+    from api.v1.views.places_amenities import *
