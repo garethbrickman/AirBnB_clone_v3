3c7ae85959a822c07a295b8990af7bf9691ff2c5
will <818@holbertonschool.com>
2020-01-30 21:30:14 +0000

    [BUGFIX] Get rid of temporary file

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -14,4 +14,3 @@ if True:
     from api.v1.views.users import *
     from api.v1.views.places import *
     from api.v1.views.places_reviews import *
-    from api.v1.views.places_amenities import *
